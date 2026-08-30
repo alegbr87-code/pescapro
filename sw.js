8f7d0217976b560a1e9b24bd92194aeeaa860885
alegbr87-code <alegbr87@gmail.com>
2026-08-30 10:43:33 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pescapro-v15-modals-buttons-gps';
+const CACHE_NAME = 'pescapro-v17-multispecies-basket';
 const urlsToCache = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', event => {
