ca5e96a3f4e0ffeafb267f335753fcaa04b33208
alegbr87-code <alegbr87@gmail.com>
2026-08-30 11:21:43 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pescapro-v20-edit-modal-data-fix';
+const CACHE_NAME = 'pescapro-v21-spot-georeferencing-map';
 const urlsToCache = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', event => {
