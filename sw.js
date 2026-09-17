3aed4cea69fa0ebe5aaff900c7e66e0b108d000e
alegbr87-code <alegbr87@gmail.com>
2026-09-17 20:47:24 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pescapro-v22-notturna';
+const CACHE_NAME = 'pescapro-v23-notturna';
 const urlsToCache = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', event => {
