80a832d747cdd8dc7cf5a4be57fc4a584bd5bcea
alegbr87-code <alegbr87@gmail.com>
2026-09-19 16:32:41 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pescapro-v32-radar-timeline';
+const CACHE_NAME = 'pescapro-v33-esche-tecniche-mappe';
 const urlsToCache = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', event => {
