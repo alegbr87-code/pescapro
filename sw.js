78ef4fac67f7035ac930fd66f6add4bd45b270b5
alegbr87-code <alegbr87@gmail.com>
2026-09-19 15:49:06 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pescapro-v31-radar-esri';
+const CACHE_NAME = 'pescapro-v32-radar-timeline';
 const urlsToCache = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', event => {
